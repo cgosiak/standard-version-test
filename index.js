5d8f3d6ec783ef564f681364f41ff64284463a15
Caleb Gosiak <Caleb.Gosiak@genmills.com>
2018-10-14 14:29:15 -0500

    feat(json): added json output

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.status(200).send('Welcome!');
+    res.status(200).json({
+        message: 'Welcome'
+    });
 });
 
 app.listen(port, () => {
