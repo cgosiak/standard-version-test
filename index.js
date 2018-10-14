f0f028baf22e65107d56eae83d9a20412b295bda
Caleb Gosiak <Caleb.Gosiak@genmills.com>
2018-10-14 14:11:04 -0500

    feat(environment-variable): added an environment variable for PORT

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.status(200).send('Welcome!');
 });
 
-app.listen(3000, () => {
-    console.log('Application Listening in Port 3000');
+app.listen(port, () => {
+    console.log(`Application Listening in Port ${port}`);
 });
